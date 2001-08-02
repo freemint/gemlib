720bc5ebc66c36dab6298f6c279a3dff53f1414b
Bishop Fox <altf4@phx2600.org>
2001-08-02 03:59:49 +0000

    fixed, intin[3] was used instead of [2]

--- gemlib/v_set_app_buff.c
+++ gemlib/v_set_app_buff.c
@@ -4,8 +4,8 @@
 void
 v_set_app_buff (short handle, void *buf_p, short size)
 {
-	*((void**)(&vdi_intin[0])) = buf_p;
-	vdi_intin[3]               = size;
+	vdi_intin_ptr(0) = buf_p;
+	vdi_intin    [2] = size;
 	
 	VDI_TRAP_ESC (vdi_params, handle, -1,6, 0,3);
 }
