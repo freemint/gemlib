3b2322f07c89b80fd3c23f6e69799aae64fbbf2c
Bishop Fox <altf4@phx2600.org>
2001-08-02 04:00:45 +0000

    removed double code

--- gemlib/vt_origin.c
+++ gemlib/vt_origin.c
@@ -11,6 +11,5 @@ vt_origin (short handle, short xorigin, short yorigin)
 	vdi_intin[0] = xorigin;
 	vdi_intin[1] = yorigin;
 	
-	vdi (&vdi_params);
 	VDI_TRAP_ESC (vdi_params, handle, 5,83, 0,2);
 }
