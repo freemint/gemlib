72e79412ea2d28889ac33286e9cc3ebf463083f0
Konrad Kokoszkiewicz <drac030@krap.pl>
2002-02-10 18:33:53 +0000

    Config tool (sources)

---
+++ gemma/configtool/src/gemma.h
@@ -0,0 +1,24 @@
+/* Resource file indices for GEMMA. */
+
+#define WINDOW           0   /* Form/Dialog-box */
+#define BUTTONRELEASE    2   /* FTEXT in tree WINDOW */
+#define AVBUFRELEASE     5   /* FTEXT in tree WINDOW */
+#define CHECKPOINT       7   /* BUTTON in tree WINDOW */
+#define DRAWZOOM         8   /* BUTTON in tree WINDOW */
+#define MOVEBOXES        9   /* BUTTON in tree WINDOW */
+#define COMPENSATION     10  /* BUTTON in tree WINDOW */
+#define FORMALERT        11  /* BUTTON in tree WINDOW */
+#define DEBUGGING        12  /* BUTTON in tree WINDOW */
+#define OK               13  /* BUTTON in tree WINDOW */
+#define CANCEL           14  /* BUTTON in tree WINDOW */
+#define FSELECTSLB       15  /* BUTTON in tree WINDOW */
+
+#define MUSTRESTART      0   /* Alert-string */
+
+#define IMMEDIATELY      1   /* Alert-string */
+
+#define CANNOTLOAD       2   /* Alert-string */
+
+#define PNAME            3   /* Free String */
+
+#define WNAME            4   /* Free String */
