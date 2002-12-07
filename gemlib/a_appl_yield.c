eca9ac42c4b9b0513ccc777918cacab4a8c2a871
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2002-12-07 22:36:44 +0000

    use if AES_PARAMS macro

--- gemlib/a_appl_yield.c
+++ gemlib/a_appl_yield.c
@@ -15,19 +15,7 @@
 short 
 mt_appl_yield(short *global_aes)
 {
-   	static short 	aes_control[AES_CTRLMAX]={17,0,1,0,0};
-	short		    aes_intin[AES_INTINMAX],
-				    aes_intout[AES_INTOUTMAX];
-	long		    aes_addrin[AES_ADDRINMAX],
-				    aes_addrout[AES_ADDROUTMAX];
-
-	AESPB aes_params;
-  	aes_params.control = &aes_control[0];   /* AES Control Array             */
-  	aes_params.global  = &global_aes[0];    /* AES Global Array              */
-  	aes_params.intin   = &aes_intin[0];     /* input integer array           */
-  	aes_params.intout  = &aes_intout[0];    /* output integer array          */
-  	aes_params.addrin  = &aes_addrin[0];    /* input address array           */
-  	aes_params.addrout = &aes_addrout[0];   /* output address array          */
+	AES_PARAMS({17,0,1,0,0});
                     
 	AES_TRAP(aes_params);
 
