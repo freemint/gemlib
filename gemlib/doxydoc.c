f5f1979d2b031e3b57e02a2eb68dae374f7cc3d0
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2002-12-07 22:29:28 +0000

    definition of GROUP and other stuff for doxygen only

---
+++ gemlib/doxydoc.c
@@ -0,0 +1,120 @@
+/*
+ *  $Id$
+ */
+
+/* This file just contains some documentation for doxygen,
+   nothing to be compiled here */
+
+/** @defgroup AES AES  Function Reference
+ *  @{
+ */
+
+/** @defgroup a_appl Application Services Library
+ *
+ *  @brief initialization and interprocess communication
+ *
+ *  The Application Services Library provides general use functions used in
+ *  locating and working with other resident applications in addition to
+ *  providing AES initialization and termination code.
+ */
+
+
+/** @defgroup a_evnt Event Library
+ *
+ *  @brief wait for user response
+ *
+ *  The Event Library consists of a group of system calls which are used to
+ *  monitor system messages including mouse clicks, keyboard usage, menu bar
+ *  interaction, timer calls, and mouse tracking.
+ */
+
+/** @defgroup a_form Form Library
+ *
+ *  @brief user interaction with dialogs & error messages
+ *
+ *  The Form Library contains utility functions for the use and control of
+ *  dialog boxes, alert boxes, and user input.
+ */
+
+/** @defgroup a_fsel File Selector Library
+ *
+ *  @brief standard file selector
+ *
+ *  The File Selector Library contains two functions for displaying the
+ *  system file selector (or currently installed alternate file selector) and
+ *  prompting the user to select a file.
+ */
+
+/** @defgroup a_graf Graphics Library
+ *
+ *  @brief special mouse functions
+ *
+ *  The Graphics Library provides applications with a variety of utility
+ *  functions which serve to provide common screen effects, mouse control, and
+ *  the obtaining of basic screen attributes.
+ */
+
+/** @defgroup a_menu Menu Library
+ *
+ *  @brief create or modify drop-down menu
+ *
+ *  The Menu Library assists in the handling of system menu bars and popup
+ *  menus. In addition, individual control of menu items can also be handled
+ *  through these functions.
+ */
+
+/** @defgroup a_objc Object Library
+ *
+ *  @brief display or modify object trees
+ *
+ *  The Object Library is responsible for the drawing and manipulation of AES
+ *  objects such as boxes, strings, icons, etc. See earlier in this chapter
+ *  for a complete discussion of AES objects.
+ */
+
+/** @defgroup a_rsrc Resource Library
+ *
+ *  @brief resource files
+ *
+ *  The Resource Library is responsibe for the loading/unloading of resource
+ *  files and the manipulation of resource objects in memory.
+ */
+
+/** @defgroup a_scrp Scrap Library
+ *
+ *  @brief interprocess cut & paste operations
+ *
+ *  The Scrap Library is used to maintain the location of the clipboard
+ *  directory used for interprocess data exchange.
+ */
+
+/** @defgroup a_shel Shell Library
+ *
+ *  @brief AES shell functions
+ *
+ *  The Shell Library contains several miscellaneous functions most often
+ *  used by the GEM Desktop and other 'Desktop-like' applications. Other
+ *  applications may, however, need specific functions of the Shell Library
+ *  for various tasks.
+ */
+
+/** @defgroup a_wind Window Library
+ *
+ *  @brief window frame interface
+ *
+ *  The Window Library is responsible for the displaying and maintenance of
+ *  AES windows.
+ */
+
+/** @defgroup a_util Utility Library
+ *
+ *  @brief some usefull utilities function
+ *
+ *  
+ */
+
+/**@}*/
+
+/** @defgroup VDI VDI  Function Reference
+ *  todo (any contribution is welcome)...
+ */
