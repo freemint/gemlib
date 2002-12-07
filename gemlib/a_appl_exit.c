2b752c4a17f69a68325a328756e670dd182a2b7d
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2002-12-07 22:33:23 +0000

    use of AES_PARAMS macro; add doc

--- gemlib/a_appl_exit.c
+++ gemlib/a_appl_exit.c
@@ -4,30 +4,26 @@
 
 #include "gem_aesP.h"
 
-/** Application Exit
+/** should be called at the termination of any program initialized
+ *  with appl_init().
  *
  *  @param global_aes global AES array
- *  @return add return description here
  *
- *  add detailled description here.  
+ *  @return 0 if an error occurred or non-zero otherwise.
+ *
+ *  @since All AES versions.
+ *
+ *  @sa mt_appl_init()
+ *
+ *  The proper procedure for handling an error from this
+ *  function is currently undefined.
+ *
  */
 
 short 
 mt_appl_exit(short *global_aes)
 {
-   	static short 	aes_control[AES_CTRLMAX]={19,0,1,0,0};
-	short		    aes_intin[AES_INTINMAX],
-				    aes_intout[AES_INTOUTMAX];
-	long		    aes_addrin[AES_ADDRINMAX],
-				    aes_addrout[AES_ADDROUTMAX];
-
-	AESPB aes_params;
-  	aes_params.control = &aes_control[0];   /* AES Control Array             */
-  	aes_params.global  = &global_aes[0];    /* AES Global Array              */
-  	aes_params.intin   = &aes_intin[0];     /* input integer array           */
-  	aes_params.intout  = &aes_intout[0];    /* output integer array          */
-  	aes_params.addrin  = &aes_addrin[0];    /* input address array           */
-  	aes_params.addrout = &aes_addrout[0];   /* output address array          */
+	AES_PARAMS({19,0,1,0,0});
                     
 	AES_TRAP(aes_params);
 
