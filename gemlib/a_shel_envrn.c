9c27468ad0e844d5ed66299987a5ec1b0bf50793
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2002-12-08 17:42:55 +0000

    merge mgemlib and gemlib; add doc

--- gemlib/a_shel_envrn.c
+++ gemlib/a_shel_envrn.c
@@ -1,13 +1,55 @@
+/*
+ *  $Id$
+ */
+
 #include "gem_aesP.h"
 
+/** searches the current environment string for a specific variable.
+ *
+ *  @param result points to a character pointer which will be filled in
+ *         with the address of the first character in the environment
+ *         string following the string given by \p param. If the string
+ *         given by \p param is not found, value will be filled in with
+ *         NULL. For instance, suppose the current environment looked
+ *         like this:  PATH=C:\\;D:\\;E:\\ \n
+ *         A call made to mt_shel_envrn() with \p param pointing to the
+ *         string 'PATH=' would set the pointer pointed to by value to
+ *         the string 'C:\\;D:\\;E:\\' above.
+ *  @param param 
+ *  @param global_aes global AES array
+ *
+ *  @return currently always returns 1.
+ *
+ *  @since All AES versions.
+ *
+ *  AES versions prior to 1.4 only accepted semi-colons as
+ *  separators between multiple 'PATH='arguments. Newer
+ *  versions accept commas as well.
+ *
+ *  The character string pointed to by name should include the
+ *  name of the variable and the equals sign.
+ */
 
-short
-shel_envrn (char **result, const char *param)
+short 
+mt_shel_envrn(char **result, const char *param, short *global_aes)
 {
+	AES_PARAMS(125,0,1,2,0);
+                    
 	aes_addrin[0] = (long)result;
 	aes_addrin[1] = (long)param;
-	
-	AES_TRAP (aes_params, 125, 0,1,2,0);
-	
+
+	AES_TRAP(aes_params);
+
 	return aes_intout[0];
 }
+
+
+#ifdef shel_envrn
+#undef shel_envrn
+#endif
+short 
+shel_envrn(char **result, const char *param)
+{
+	return(mt_shel_envrn(result,param, aes_global));
+}
+
--- gemlib/a_shel_help.c
+++ gemlib/a_shel_help.c
@@ -1,14 +1,44 @@
+/*
+ *  $Id$
+ */
+
 #include "gem_aesP.h"
 
+/** 
+ *
+ *  @param sh_hmode
+ *  @param sh_hfile
+ *  @param sh_hkey
+ *  @param global_aes global AES array
+ *
+ *  @return 
+ *
+ *  @since 
+ *
+ *  @sa 
+ *
+ */
 
-short
-shel_help (short sh_hmode, const char *sh_hfile, const char *sh_hkey)
+short 
+mt_shel_help(short sh_hmode, const char *sh_hfile, const char *sh_hkey, short *global_aes)
 {
+	AES_PARAMS(128,1,1,2,0);
+                    
 	aes_intin[0]  = sh_hmode;
 	aes_addrin[0] = (long)sh_hfile;
 	aes_addrin[1] = (long)sh_hkey;
-	
-	AES_TRAP (aes_params, 128, 1,1,2,0);
-	
+
+	AES_TRAP(aes_params);
+
 	return aes_intout[0];
 }
+
+#ifdef shel_help
+#undef shel_help
+#endif
+short 
+shel_help(short sh_hmode, const char *sh_hfile, const char *sh_hkey)
+{
+	return(mt_shel_help(sh_hmode, sh_hfile, sh_hkey, aes_global));
+}
+
