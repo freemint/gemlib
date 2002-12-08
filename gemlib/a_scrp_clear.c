e30487f5fd168bd58728382ca80a47c8d61ed153
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2002-12-08 18:28:02 +0000

    merge mgemlib and gemlib; add doc

--- gemlib/a_scrp_clear.c
+++ gemlib/a_scrp_clear.c
@@ -1,10 +1,37 @@
+/*
+ *  $Id$
+ */
+
 #include "gem_aesP.h"
 
+/** 
+ *
+ *  @param global_aes global AES array
+ *
+ *  @return 
+ *
+ *  @since 
+ *
+ *  @sa 
+ *
+ */
 
-short
-scrp_clear (void)
+short 
+mt_scrp_clear( short *global_aes)
 {
-	AES_TRAP (aes_params, 82, 0,1,0,0);
-	
+	AES_PARAMS(82,0,1,0,0);
+
+	AES_TRAP(aes_params);
+
 	return aes_intout[0];
 }
+
+#ifdef scrp_clear
+#undef scrp_clear
+#endif
+short 
+scrp_clear(void)
+{
+	return(mt_scrp_clear(aes_global));
+}
+
--- gemlib/a_scrp_write.c
+++ gemlib/a_scrp_write.c
@@ -1,12 +1,50 @@
+/*
+ *  $Id$
+ */
+
 #include "gem_aesP.h"
 
+/** sets the location of the clipboard directory.
+ *
+ *  @param Scrappath points to a NULL-terminated path string containing
+ *         a valid drive and path specification with a closing
+ *         backslash. The following is an example of a correctly
+ *         formatted Scrappath argument: \c C:\\CLIPBRD\\
+ *  @param global_aes global AES array
+ *
+ *  @return 0 if an error occurred or non-zero otherwise.
+ *
+ *  @since All AES versions.
+ *
+ *  @sa mt_scrp_read()
+ *
+ *  The scrap directory is a global resource. This call should
+ *  only be used in two circumstances as follows: when used to
+ *  set the default location of the scrap directory using a CPX
+ *  or accessory at bootup or by the user's request.
+ *
+ *  when scrp_read() returns an error value and you need to
+ *  create the clipboard to write information to it.
+ */
 
-short
-scrp_write (const char *Scrappath)
+short 
+mt_scrp_write(const char *Scrappath, short *global_aes)
 {
+	AES_PARAMS(81,0,1,1,0);
+                    
 	aes_addrin[0] = (long)Scrappath;
-	
-	AES_TRAP (aes_params, 81, 0,1,1,0);
-	
+
+	AES_TRAP(aes_params);
+
 	return aes_intout[0];
 }
+
+#ifdef scrp_write
+#undef scrp_write
+#endif
+short 
+scrp_write(const char *Scrappath)
+{
+	return(mt_scrp_write(Scrappath, aes_global));
+}
+
