e8efca67b8a1eb12a8deb90373f2208df482097c
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2002-12-08 19:11:01 +0000

    merge mgemlib and gemlib; add doc

--- gemlib/a_rsrc_obfix.c
+++ gemlib/a_rsrc_obfix.c
@@ -1,13 +1,48 @@
+/*
+ *  $Id$
+ */
+
 #include "gem_aesP.h"
 
+/** converts an object's coordinates from
+ *  character-based to pixel-based.
+ *
+ *  @param Tree specifies the OBJECT tree containing the object...
+ *  @param Index ... to convert.
+ *  @param global_aes global AES array
+ *
+ *  @return always returns a 0.
+ *
+ *  @since All AES versions.
+ *
+ *  @sa mt_rsrc_load(), mt_rsrc_rcfix()
+ *
+ *  All objects in '.RSC' files have their coordinates based on
+ *  character positions rather than screen coordinates to allow
+ *  an object tree to be shown in any resolution. This function
+ *  converts those character coordinates to pixel coordinates
+ *  based on the current screen resolution.
+ *
+ */
 
-short
-rsrc_obfix (OBJECT *Tree, short Index)
+short 
+mt_rsrc_obfix(OBJECT *Tree, short Index, short *global_aes)
 {
+	AES_PARAMS(114,1,1,1,0);
+                    
 	aes_intin[0]  = Index;
 	aes_addrin[0] = (long)Tree;
-	
-	AES_TRAP (aes_params, 114, 1,1,1,0);
-	
+
+	AES_TRAP(aes_params);
+
 	return aes_intout[0];
+}
+
+#ifdef rsrc_obfix
+#undef rsrc_obfix
+#endif
+short 
+rsrc_obfix(OBJECT *Tree, short Index)
+{
+	return(mt_rsrc_obfix(Tree, Index, aes_global));
 }
--- gemlib/a_rsrc_saddr.c
+++ gemlib/a_rsrc_saddr.c
@@ -1,14 +1,49 @@
+/*
+ *  $Id$
+ */
+
 #include "gem_aesP.h"
 
+/** sets the address of a resource element.
+ *
+ *  @param Type specifies the type of resource element to set as
+ *         defined under mt_rsrc_gaddr()
+ *  @param Index specifies the index of the element to modify (0 based)
+ *  @param Address specifies the actual address that will be placed in
+ *         the appropriate data structure.
+ *  @param global_aes global AES array
+ *
+ *  @return 0 if an error occurred or non-zero otherwise.
+ *
+ *  @since All AES versions.
+ *
+ *  @sa mt_rsrc_gaddr(), mt_rsrc_load()
+ *
+ *  In most cases, direct manipulation of the structures
+ *  involved is quicker and easier than using this call.
+ *
+ */
 
-short
-rsrc_saddr (short Type, short Index, void *Address)
+short 
+mt_rsrc_saddr(short Type, short Index, void *Address, short *global_aes)
 {
+	AES_PARAMS(113,2,1,1,0);
+                    
 	aes_intin[0]  = Type;
 	aes_intin[1]  = Index;
 	aes_addrin[0] = (long)Address;
-	
-	AES_TRAP (aes_params, 113, 2,1,1,0);
-	
+
+	AES_TRAP(aes_params);
+
 	return aes_intout[0];
 }
+
+#ifdef rsrc_saddr
+#undef rsrc_saddr
+#endif
+short 
+rsrc_saddr(short Type, short Index, void *Address)
+{
+	return(mt_rsrc_saddr(Type, Index, Address,aes_global));
+}
+
