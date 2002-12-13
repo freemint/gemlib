6af8a3521e905377982c72f653ee9fd508fab657
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2002-12-13 21:26:21 +0000

    add x_edit group

--- gemlib/doxydoc.c
+++ gemlib/doxydoc.c
@@ -108,9 +108,21 @@
 
 /** @defgroup a_util Utility Library
  *
- *  @brief some usefull utilities function
+ *  @brief some usefull utilities function. This library doesn't contain
+ *         any AES call.
  *
- *  
+ */
+
+/** @defgroup x_edit extended Edit Library
+ *
+ *  @brief AES extended edit functions
+ *
+ *  This Edit Library is available from MagiC 5.20 onwards. As it is 
+ *  implemented as part of the shared library (SLB or SharedLib), 
+ *  one only needs to open the library with:\n
+ *  <pre>Slbopen("EDITOBJC.SLB", NULL, 0L, &slb, &slbexec);</pre>
+ *  The library installs a new AES object-type G_EDIT (37)
+ *  as well as the AES functions mt_edit_xx().
  */
 
 /**@}*/
