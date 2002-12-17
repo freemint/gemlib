4bc0188b5f4fbb01d1a003efc142cef702239c21
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2002-12-17 21:08:10 +0000

    merge mgemlib and gemlib; add doc

--- gemlib/x_objc_wdraw.c
+++ gemlib/x_objc_wdraw.c
@@ -1,22 +1,34 @@
+/*
+ *  $Id$
+ */
+
 #include "gemx.h"
 #include "gem_aesP.h"
 
+/** Draw object(tree)
+ *
+ *  @param tree as for mt_objc_draw()
+ *  @param start  as for mt_objc_draw()
+ *  @param depth  as for mt_objc_draw()
+ *  @param clip NULL => whole window
+ *  @param whandle window handle
+ *  @param global_aes global AES array
+ *
+ *  @since MagiC 5.10
+ *
+ */
 
-void
-objc_wdraw (OBJECT *tree, short start, short depth, GRECT *clip, short whandle)
+void 
+mt_objc_wdraw(OBJECT *tree, short start, short depth, GRECT *clip, short whandle, short *global_aes)
 {
+	AES_PARAMS(60,3,0,2,0);
+
 	aes_intin[0] = start;
 	aes_intin[1] = depth;
 	aes_intin[2] = whandle;
 
-	aes_addrin[0] = (long) tree;
-	aes_addrin[1] = (long) clip;
+	aes_addrin[0] = (long)tree;
+	aes_addrin[1] = (long)clip;
 
-	aes_control[0] = 60;
-	aes_control[1] = 3;
-	aes_control[2] = 0;
-	aes_control[3] = 2;
-	aes_control[4] = 0;
-
-	aes (&aes_params);
+	AES_TRAP(aes_params);
 }
--- gemlib/x_objc_xedit.c
+++ gemlib/x_objc_xedit.c
@@ -1,27 +1,45 @@
+/*
+ *  $Id$
+ */
+
 #include "gemx.h"
 #include "gem_aesP.h"
 
+/** 
+ *
+ *  @param tree 
+ *  @param obj 
+ *  @param key 
+ *  @param xpos 
+ *  @param subfn
+ *  @param r
+ *  @param global_aes global AES array
+ *
+ *  @return 
+ *
+ *  @since 
+ *
+ *
+ *
+ */
 
-short
-objc_xedit (OBJECT *tree, short obj, short key, short *xpos, short subfn, GRECT *r)
+short 
+mt_objc_xedit(OBJECT *tree, short obj, short key, short *xpos, short subfn,
+              GRECT *r, short *global_aes)
 {
+	AES_PARAMS(46,4,2,2,0);
+
 	aes_intin[0] = obj;
 	aes_intin[1] = key;
 	aes_intin[2] = *xpos;
 	aes_intin[3] = subfn;
 
-	aes_addrin[0] = (long) tree;
-	aes_addrin[1] = (long) r;
+	aes_addrin[0] = (long)tree;
+	aes_addrin[1] = (long)r;
 
-	aes_control[0] = 46;
-	aes_control[1] = 4;
-	aes_control[2] = 2;
-	aes_control[3] = 2;
-	aes_control[4] = 0;
-
-	aes (&aes_params);
+	AES_TRAP(aes_params);
 
 	*xpos = aes_intout[1];
-
+	
 	return aes_intout[0];
 }
