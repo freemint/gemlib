4e877d1daa374e4957776a5584fad44c84b94c95
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2002-12-17 19:43:57 +0000

    merge gemlib and mgemlib; add doc

--- gemlib/x_fslx_getnxtfile.c
+++ gemlib/x_fslx_getnxtfile.c
@@ -1,17 +1,36 @@
+/*
+ *  $Id$
+ */
+
 #include "gemx.h"
 #include "gem_aesP.h"
 
+/** Examine further selected files
+ *
+ *  @param fsd Descriptor that was passed at a 
+ *		   successful opening of a file selector
+ *		   window
+ *  @param fname see below
+ *  @param global_aes global AES array
+ *
+ *  @return 0, when no further file is selected, \p fname is unchanged.\n
+ *			1, when a filename after \p fname was copied.
+ *
+ *  @since mt_appl_getinfo(7) give informations on mt_fslx_xx() functions availability
+ *
+ *
+ *
+ */
 
-short
-fslx_getnxtfile (void *fsd, char *fname)
+short 
+mt_fslx_getnxtfile(void *fsd, char *fname, short *global_aes)
 {
-	aes_addrin[0] = (long) fsd;
-	aes_addrin[1] = (long) fname;
-	aes_control[0] = 192;
-	aes_control[1] = 0;
-	aes_control[2] = 1;
-	aes_control[3] = 2;
-	aes_control[4] = 0;
-	aes (&aes_params);
+	AES_PARAMS(192,0,1,2,0);
+
+	aes_addrin[0] = (long)fsd;
+	aes_addrin[1] = (long)fname;
+
+	AES_TRAP(aes_params);
+
 	return aes_intout[0];
 }
