220fd4ff2d19b1023b3e56bbcceca7e968c95abd
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2002-12-21 21:00:30 +0000

    add mt_wdlg_ functions; add doc; add mt_lbox_ functions with 1 slider

--- gemlib/gemx.h
+++ gemlib/gemx.h
@@ -21,15 +21,15 @@ __BEGIN_DECLS
 
 typedef struct
 {
-	short		mwhich;		/* Art der Ereignisse */
-	short		mx;		/* x-Koordinate des Mauszeigers */
-	short		my;		/* y-Koordinate des Mauszeigers */
-	short		mbutton; 	/* gedrÅckte Maustaste */
-	short		kstate;		/* Status der Sondertasten (kbshift) */
-	short		key;		/* Scancode der gedrÅckten Taste */
-	short		mclicks; 	/* Anzahl der Mausklicks */
-	short		reserved[9];	/* reserviert */
-	short		msg[16]; 	/* Message-Buffer */
+	short		mwhich;		/**< Type of events */
+	short		mx;			/**< X-coordinate of the mouse cursor */
+	short		my;			/**< Y-coordinate of the mouse cursor */
+	short		mbutton; 	/**< Pressed mouse button */
+	short		kstate;		/**< Status of the 'special' keys (kbshift) */
+	short		key;		/**< Scancode of the pressed key */
+	short		mclicks; 	/**< Number of mouse clicks */
+	short		reserved[9];/**< Reserved */
+	short		msg[16]; 	/**< Message-buffer */
 } EVNT;
 
 /*
@@ -723,46 +723,149 @@ void		mt_lbox_bscroll_to (LIST_BOX *box, short first, GRECT *box_rect,
 #define	lbox_get_first(a)			mt_lbox_get_afirst(a,aes_global)
 #define	lbox_set_slider(a,b,c)		mt_lbox_set_asldr(a,b,c,aes_global)
 #define	lbox_scroll_to(a,b,c,d)		mt_lbox_ascroll_to(a,b,c,d,aes_global)
+#define	mt_lbox_get_visible			mt_lbox_get_avis
+#define	mt_lbox_get_first			mt_lbox_get_afirst
+#define	mt_lbox_set_slider			mt_lbox_set_asldr
+#define	mt_lbox_scroll_to			mt_lbox_ascroll_to
 /**@}*/
 
 /******************************************************************************
  * Wdialog definitions
  */
 
+/** @addtogroup x_wdlg
+ *  @{
+ */
+
+/** an opaque structure. One should not access the 
+    structure directly. The mt_wdlg_xx functions should be used! */
 typedef void * DIALOG;
+
+/** service routine that is called, among others, by mt_wdlg_evnt().
+ *
+ *  This function may be called if an exit or touchexit 
+ *  object was clicked on (in that case \p obj is a positive object number) 
+ *  or when an event has occurred that affects the dialog (in that case 
+ *  \p obj is negative and contains a corresponding function number such as 
+ *  HNDL_CLSD, for instance).
+ *  
+ *  If \p obj is an object number (>= 0), then \p events points to 
+ *  the EVNT structure that was passed by mt_wdlg_evnt().
+ *  Otherwise \p events is basically 0L and can  not be used for addressing.
+ *  
+ *  \p clicks contains then number of mouse clicks (if \p obj is an object number)
+ *
+ *  Here is a list of event (value given in the \p obj parameter):
+ *  - HNDL_INIT (-1) : \n
+ *	  \p data is the variable passed by wdlg_create.
+ *	  If handle_exit() returns 0, mt_wdlg_create() does not
+ *	  create a dialog structure (error).
+ *	  The variable \p code is passed in \p clicks.
+ *  - HNDL_OPEN (-5) : \n
+ *    \p data is the variable passed by wdlg_open.
+ *    The variable \p code is passed in \p clicks.
+ *  - HNDL_CLSD (-3) : \n
+ *    \p data is \p user_data. If handle_exit() returns 0,
+ *    the dialog will be closed -- mt_wdlg_evnt() returns 0
+ *    \p events points to the EVNT structure passed by
+ *    mt_wdlg_evnt(). 
+ *  - HNDL_MOVE (-9) : \n
+ *    \p data is \p user_data. If handle_exit() returns 0,
+ *    the dialog will be closed -- mt_wdlg_evnt() returns 0.
+ *    \p events points to the EVNT structure passed by
+ *    mt_wdlg_evnt().
+ *  - HNDL_TOPW (-10) : \n
+ *    \p data is \p user_data. If handle_exit() returns 0,
+ *    the dialog will be closed -- mt_wdlg_evnt() returns 0.
+ *    \p events points to the EVNT structure passed by 
+ *    mt_wdlg_evnt().
+ *  - HNDL_UNTP (-11) : \n
+ *    \p data is \p user_data. If handle_exit() returns 0,
+ *    the dialog will be closed -- mt_wdlg_evnt() returns 0.
+ *    \p events points to the EVNT structure passed by 
+ *    mt_wdlg_evnt().
+ *  - HNDL_EDIT (-6) : \n
+ *    \p data points to a word with the key code.
+ *    If handle_exit() returns 1, the key press will be
+ *    evaluated, if 0 ignored.
+ *    \p events points to the EVNT structure passed by
+ *    mt_wdlg_evnt().  
+ *  - HNDL_EDDN (-7) : \n
+ *    \p data points to a word with the key code.
+ *    \p events points to the EVNT structure passed by
+ *    mt_wdlg_evnt().
+ *  - HNDL_EDCH (-8) : \n
+ *    \p data points to a word with the object number of
+ *    the new editable field.
+ *  - HNDL_MESG (-2) : \n
+ *    \p data is \p user_data. If handle_exit() returns 0,
+ *    the dialog will be closed -- mt_wdlg_evnt() returns 0.
+ *    \p events points to the EVNT structure passed by
+ *    mt_wdlg_evnt().	\n
+ *    HNDL_MESG is only passed if a message code between
+ *    20 and 39 was received that is not handled by other
+ *    opcodes.
+ *    Is required for iconification, for instance.\n
+ *    Warning: This opcode is only present from MagiC 4.5
+ *    of 18.4.96 
+ *  .
+ *  Of these function numbers one only has to react to HNDL_CLSD. All other 
+ *  events need only be paid attention to when needed.\n
+ *  If handle_exit is called with an unknown function number in <obj>, or 
+ *  one of the above function numbers is to be ignored, then 1 has to be 
+ *  returned.
+ * 
+ *  The parameters are passed via the stack and the routine may alter 
+ *  registers d0-d2/a0-a2.
+ */
 typedef short __CDECL (*HNDL_OBJ)(DIALOG *dialog, EVNT *events, short obj, short clicks, void *data);
 
-DIALOG *wdlg_create (HNDL_OBJ handle_exit, OBJECT *tree, void *user_data, short code, void *data, short flags);
-short	wdlg_open (DIALOG *dialog, char *title, short kind, short x, short y, short code, void *data);
-short	wdlg_close (DIALOG *dialog, short *x, short *y);
-short	wdlg_delete (DIALOG *dialog);
-short	wdlg_get_tree (DIALOG *dialog, OBJECT **tree, GRECT *r);
-short	wdlg_get_edit (DIALOG *dialog, short *cursor);
-void *	wdlg_get_udata (DIALOG *dialog);
-short	wdlg_get_handle (DIALOG *dialog);
-short	wdlg_set_edit (DIALOG *dialog, short obj);
-short	wdlg_set_tree (DIALOG *dialog, OBJECT *tree);
-short	wdlg_set_size (DIALOG *dialog, GRECT *size);
-short	wdlg_set_iconify (DIALOG *dialog, GRECT *g, char *title, OBJECT *tree, short obj);
-short	wdlg_set_uniconify (DIALOG *dialog, GRECT *g, char *title, OBJECT *tree);
-short	wdlg_evnt (DIALOG *dialog, EVNT *events );
-void	wdlg_redraw (DIALOG *dialog, GRECT *rect, short obj, short depth);
-
+DIALOG * mt_wdlg_create			(HNDL_OBJ handle_exit, OBJECT *tree, void *user_data, short code, void *data, short flags, short *global);
+short	 mt_wdlg_open			(DIALOG *dialog, char *title, short kind, short x, short y, short code, void *data, short *global);
+short	 mt_wdlg_close			(DIALOG *dialog, short *x, short *y, short *global);
+short	 mt_wdlg_delete			(DIALOG *dialog, short *global);
+short	 mt_wdlg_get_tree		(DIALOG *dialog, OBJECT **tree, GRECT *r, short *global);
+short	 mt_wdlg_get_edit		(DIALOG *dialog, short *cursor, short *global);
+void *	 mt_wdlg_get_udata		(DIALOG *dialog, short *global);
+short	 mt_wdlg_get_handle		(DIALOG *dialog, short *global);
+short	 mt_wdlg_set_edit		(DIALOG *dialog, short obj, short *global);
+short	 mt_wdlg_set_tree		(DIALOG *dialog, OBJECT *tree, short *global);
+short	 mt_wdlg_set_size		(DIALOG *dialog, GRECT *size, short *global);
+short	 mt_wdlg_set_iconify	(DIALOG *dialog, GRECT *g, char *title, OBJECT *tree, short obj, short *global);
+short	 mt_wdlg_set_uniconify	(DIALOG *dialog, GRECT *g, char *title, OBJECT *tree, short *global);
+short	 mt_wdlg_evnt			(DIALOG *dialog, EVNT *events, short *global );
+void	 mt_wdlg_redraw			(DIALOG *dialog, GRECT *rect, short obj, short depth, short *global);
+#define wdlg_create(a,b,c,d,e,f)	mt_wdlg_create(a,b,c,d,e,f,aes_global)
+#define wdlg_open(a,b,c,d,e,f,g)	mt_wdlg_open(a,b,c,d,e,f,g,aes_global)
+#define wdlg_close(a,b,c)			mt_wdlg_close(a,b,c,aes_global)
+#define wdlg_delete(a)				mt_wdlg_delete(a,aes_global)
+#define wdlg_get_tree(a,b,c)		mt_wdlg_get_tree(a,b,c,aes_global)
+#define wdlg_get_edit(a,b)			mt_wdlg_get_edit(a,b,aes_global)
+#define wdlg_get_udata(a)			mt_wdlg_get_udata(a,aes_global)
+#define wdlg_get_handle(a)			mt_wdlg_get_handle(a,aes_global)
+#define wdlg_set_edit(a,b)			mt_wdlg_set_edit(a,b,aes_global)
+#define wdlg_set_tree(a,b)			mt_wdlg_set_tree(a,b,aes_global)
+#define wdlg_set_size(a,b)			mt_wdlg_set_size(a,b,aes_global)
+#define wdlg_set_iconify(a,b,c,d,e)	mt_wdlg_set_iconify(a,b,c,d,e,aes_global)
+#define wdlg_set_uniconify(a,b,c,d)	mt_wdlg_set_uniconify(a,b,c,d,aes_global)
+#define wdlg_evnt(a,b)				mt_wdlg_evnt(a,b,aes_global)
+#define wdlg_redraw(a,b,c,d)		mt_wdlg_redraw(a,b,c,d,aes_global)
 
 /* Definitions for <flags> */
-#define  WDLG_BKGD   1           /* Permit background operation */
+#define  WDLG_BKGD   1           /**< Permit background operation */
 
 /* Function numbers for <obj> with handle_exit(...) */
-#define  HNDL_INIT   -1          /* Initialise dialog */
-#define  HNDL_MESG   -2          /* Initialise dialog */
-#define  HNDL_CLSD   -3          /* Dialog window was closed */
-#define  HNDL_OPEN   -5          /* End of dialog initialisation (second  call at end of wdlg_init) */
-#define  HNDL_EDIT   -6          /* Test characters for an edit-field */
-#define  HNDL_EDDN   -7          /* Character was entered in edit-field */
-#define  HNDL_EDCH   -8          /* Edit-field was changed */
-#define  HNDL_MOVE   -9          /* Dialog was moved */
-#define  HNDL_TOPW   -10         /* Dialog-window has been topped */
-#define  HNDL_UNTP   -11         /* Dialog-window is not active */
+#define  HNDL_INIT   -1          /**< Initialise dialog */
+#define  HNDL_MESG   -2          /**< Initialise dialog */
+#define  HNDL_CLSD   -3          /**< Dialog window was closed */
+#define  HNDL_OPEN   -5          /**< End of dialog initialisation (second  call at end of wdlg_init) */
+#define  HNDL_EDIT   -6          /**< Test characters for an edit-field */
+#define  HNDL_EDDN   -7          /**< Character was entered in edit-field */
+#define  HNDL_EDCH   -8          /**< Edit-field was changed */
+#define  HNDL_MOVE   -9          /**< Dialog was moved */
+#define  HNDL_TOPW   -10         /**< Dialog-window has been topped */
+#define  HNDL_UNTP   -11         /**< Dialog-window is not active */
+/**@}*/
 
 
 /* 
