64fa34db44e60575f53070a374f389d3dabef1a0
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2002-12-28 18:59:08 +0000

    remove comments in AES_PARAMS macro

--- gemlib/gem_aesP.h
+++ gemlib/gem_aesP.h
@@ -35,19 +35,19 @@ _aes_trap (AESPB * aespb)
 
 
 #define AES_PARAMS(a,b,c,d,e) \
-	static short    aes_control[AES_CTRLMAX]={a,b,c,d,e};    \
-	short			aes_intin[AES_INTINMAX],                  \
-					aes_intout[AES_INTOUTMAX];                \
-	long			aes_addrin[AES_ADDRINMAX],                \
-					aes_addrout[AES_ADDROUTMAX];              \
-                                          \
-	AESPB aes_params;                     \
-  	aes_params.control = &aes_control[0]; /* AES Control Array             */ \
-  	aes_params.global  = &global_aes[0];  /* AES Global Array              */ \
-  	aes_params.intin   = &aes_intin[0];   /* input integer array           */ \
-  	aes_params.intout  = &aes_intout[0];  /* output integer array          */ \
-  	aes_params.addrin  = &aes_addrin[0];  /* input address array           */ \
-  	aes_params.addrout = &aes_addrout[0]  /* output address array          */ \
+	static short    aes_control[AES_CTRLMAX]={a,b,c,d,e}; \
+	short			aes_intin[AES_INTINMAX],			  \
+					aes_intout[AES_INTOUTMAX];			  \
+	long			aes_addrin[AES_ADDRINMAX],			  \
+					aes_addrout[AES_ADDROUTMAX];		  \
+ 														  \
+	AESPB aes_params;									  \
+  	aes_params.control = &aes_control[0];				  \
+  	aes_params.global  = &global_aes[0];				  \
+  	aes_params.intin   = &aes_intin[0]; 				  \
+  	aes_params.intout  = &aes_intout[0];				  \
+  	aes_params.addrin  = &aes_addrin[0];				  \
+  	aes_params.addrout = &aes_addrout[0]
 
 
 #endif /* _GEM_AES_P_ */
