48e93e5b2cefba36f2f9264436813e70ac5a5cf6
Standa Opichal <opichals@gmail.com>
2003-03-24 22:49:16 +0000

    The TOS4.04 VDI requires the intout array to be set. Otherwise it crashes.

--- gemlib/vsl_ends.c
+++ gemlib/vsl_ends.c
@@ -24,7 +24,8 @@ vsl_ends (short handle, short begstyle, short endstyle)
 #if USE_LOCAL_VDIPB
 	short vdi_control[VDI_CNTRLMAX]; 
 	short vdi_intin[2];   
-	VDI_PARAMS(vdi_control, vdi_intin, 0L, 0L, 0L );
+	short vdi_intout[2]; 
+	VDI_PARAMS(vdi_control, vdi_intin, 0L, vdi_intout, 0L );
 #endif
 	
 	vdi_intin[0] = begstyle;
