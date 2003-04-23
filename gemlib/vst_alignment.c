5dd5f03f555233bcb3b1ab9ae736bce6a07387fa
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2003-04-23 15:26:28 +0000

    add CHECK_NULLPTR option

--- gemlib/vst_alignment.c
+++ gemlib/vst_alignment.c
@@ -20,8 +20,10 @@
  *         - TA_BOTTOM (3) : bottom
  *         - TA_DESCENT (4) : descent
  *         - TA_TOP (5) : top
- *  @param hout selected horizontal alignment
- *  @param vout selected vertical alignment
+ *  @param hout selected horizontal alignment \n
+ *         [option CHECK_NULLPTR] hout may be NULL
+ *  @param vout selected vertical alignment \n
+ *         [option CHECK_NULLPTR] vout may be NULL
  *
  *  @since all VDI versions
  *
@@ -42,6 +44,12 @@ vst_alignment (short handle, short hin, short vin, short *hout, short *vout)
 
 	VDI_TRAP (vdi_params, handle, 39, 0,2);
 
+#if CHECK_NULLPTR
+	if (hout)
+#endif
 	*hout = vdi_intout[0];
+#if CHECK_NULLPTR
+	if (vout)
+#endif
 	*vout = vdi_intout[1];
 }
