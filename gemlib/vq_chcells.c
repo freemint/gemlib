69e64dc513ba2f68433cfdea86f3177824dc39bc
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2003-04-27 13:22:12 +0000

    add CHECK_NULLPTR option

--- gemlib/vq_chcells.c
+++ gemlib/vq_chcells.c
@@ -7,9 +7,12 @@
 /** This call returns the number of columns and rows/lines of the text screen
  *
  *  @param handle Device handle
- *  @param n_rows number of lines
- *  @param n_cols number of columns \n
- *         If \p n_rows and \p n_cols are 0, the device does not support
+ *  @param n_rows number of lines \n
+ *         [option CHECK_NULLPTR] n_rows may be NULL
+ *  @param n_cols number of columns  \n
+ *         [option CHECK_NULLPTR] n_cols may be NULL\n
+ *  
+ *  @note  If \p n_rows and \p n_cols are 0, the device does not support
  *         text mode.
  *
  *  @since all VDI versions
@@ -27,6 +30,12 @@ vq_chcells (short handle, short *n_rows, short *n_cols)
 	
 	VDI_TRAP_ESC (vdi_params, handle, 5,1, 0,0);
 	
+#if CHECK_NULLPTR
+	if (n_rows)
+#endif
 	*n_rows = vdi_intout[0];
+#if CHECK_NULLPTR
+	if (n_cols)
+#endif
 	*n_cols = vdi_intout[1];
 }
