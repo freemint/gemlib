12649145459995aa2747cc3fd668ed8ec2bf49bc
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2003-05-11 14:35:50 +0000

    add 'volatile' keywords for aes_intout[] and aes_ptsout[].
    Is there another way to tell GCC that AES_TRAP(aespb) may
    alter data in aespb (aespb.intout and aespb.ptsout) ?

--- gemlib/gem_aesP.h
+++ gemlib/gem_aesP.h
@@ -36,18 +36,18 @@ _aes_trap (AESPB * aespb)
 
 #define AES_PARAMS(a,b,c,d,e) \
 	static short    aes_control[AES_CTRLMAX]={a,b,c,d,e}; \
-	short			aes_intin[AES_INTINMAX],			  \
-					aes_intout[AES_INTOUTMAX];			  \
-	long			aes_addrin[AES_ADDRINMAX],			  \
-					aes_addrout[AES_ADDROUTMAX];		  \
+	short			aes_intin[AES_INTINMAX];			  \
+	volatile short	aes_intout[AES_INTOUTMAX];			  \
+	long			aes_addrin[AES_ADDRINMAX];			  \
+	volatile long	aes_addrout[AES_ADDROUTMAX];		  \
  														  \
 	AESPB aes_params;									  \
   	aes_params.control = &aes_control[0];				  \
   	aes_params.global  = &global_aes[0];				  \
   	aes_params.intin   = &aes_intin[0]; 				  \
-  	aes_params.intout  = &aes_intout[0];				  \
+  	(volatile short *)aes_params.intout  = &aes_intout[0];\
   	aes_params.addrin  = &aes_addrin[0];				  \
-  	aes_params.addrout = &aes_addrout[0]
+  	(volatile long *)aes_params.addrout = &aes_addrout[0]
 
 
 #endif /* _GEM_AES_P_ */
