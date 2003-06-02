f95d3b580dc12488da93887ab4c5a77effb2f2ce
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2003-06-02 20:44:36 +0000

    [DOC] add doc (contrib. Rainer Seitel) fixed name filling: the vdi_array2str function added a NULL byte at the end of the string, so vdi_array2str can't be used here (contrib. Rainer Seitel)

--- gemlib/vqt_name.c
+++ gemlib/vqt_name.c
@@ -15,6 +15,8 @@
  *  @since all VDI versions; extended since NVDI 3.00 (add 33th byte to
  *  name for "bitmap/vector font" flag)
  *
+ *  @note if the name is 32 characters long, \p name is not 0 terminated !
+ *  @note some device driver returns -1 for their not available system font
  */
 
 short
@@ -23,14 +25,25 @@ vqt_name (short handle, short element, char *name)
 #if USE_LOCAL_VDIPB
 	short vdi_control[VDI_CNTRLMAX]; 
 	short vdi_intout[VDI_INTOUTMAX]; 
+	short len=33;
+	short *src;
+	char  *des;
 	VDI_PARAMS(vdi_control, &element, 0L, vdi_intout, vdi_dummy );
 #else
 	vdi_intin[0] = element;
 #endif
 	
+	vdi_intout[33] = 0;
+	
 	VDI_TRAP (vdi_params, handle, 130, 0,1);
 
-	vdi_array2str (vdi_intout + 1, name, 33);
+	/* vdi_array2str-like but without adding a NULL byte at the end */
+	src = vdi_intout + 1;
+	des = name;
+	while (len > 0) {
+		*(des++) = (char) *(src++);
+		len--;
+	}
 	
 	return vdi_intout[0];
 }
