e883e99e83629b9a1e449db4f60602559fbcfbd7
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2004-08-18 19:21:57 +0000

    documentation updated

--- gemlib/a_appl_init.c
+++ gemlib/a_appl_init.c
@@ -14,39 +14,47 @@
  *			application. If successful, the global identifier should
  *			be stored in a global variable for later use.
  *			GEM applications may not make any AES or VDI calls unless
- *			appl_init() succeeds.
+ *			mt_appl_init() succeeds.
  *			Besides the return value, the AES fills in the
- *			application's global array as following:
-<pre>
-       Name       global[x] Meaning
-
-       _AESversion    0     AES version number.
-
-       _AESnumapps    1     Number of concurrent applications
-                            possible (normally 1). MultiTOS
-                            will return -1.
-
-       _AESapid       2     Application identifier (same as
-                            appl_init() return value).
-
-       _AESappglobal  3-4   LONG global available for use by the
-                            application.
-
-       _AESrscfile    5-6   Pointer to the base of the resource
-                            loaded via rsrc_load().
-
-       (reserved)     7-12  Reserved
-
-       _AESmaxchar    13    Current maximum character used by the
-                            AES to do vst_height() prior to
-                            writing to the screen. This entry is
-                            only present as of AES version 0x0400.
-
-       _AESminchar    14    Current minimum character used by the
-                            AES to do vst_height() prior to
-                            writing to the screen. This entry is
-                            only present as of AES version 0x0400.
-</pre>
+ *			application's global array ( \a global_aes) as following:
+ * <table>
+ * <tr><td> Name <td> index of \a global_aes[] <td> Meaning
+ *
+ * <tr><td> mt_AESversion()  <td>  0  <td>
+ *                           AES version number.
+ *
+ * <tr><td>  mt_AESnumapps() <td>	1	 <td>
+ *                            Number of concurrent applications
+ *							  possible (normally 1). MultiTOS
+ *							  will return -1.
+ *
+ * <tr><td>  mt_AESapid()	<td>	2	  <td>
+ *                           Application identifier (same as
+ *		    			     mt_appl_init() return value).
+ *
+ * <tr><td> mt_AESappglobal() <td> 3-4   <td>
+ *                           LONG global available for use by the
+ *		    			     application.
+ *
+ * <tr><td> mt_AESrscfile()<td>	5-6  <td>
+ *                            Pointer to the base of the resource
+ *		    			      loaded via mt_rsrc_load().
+ *
+ * <tr><td> (reserved)   <td>  7-12  <td>
+ *                           Reserved
+ *
+ * <tr><td> mt_AESmaxchar()  <td>  13	 <td>
+ *                           Current maximum character used by the
+ *							 AES to do vst_height() prior to
+ *							 writing to the screen. This entry is
+ *							 only present as of AES version 0x0400.
+ *
+ * <tr><td> mt_AESminchar() <td>	14    <td>
+ *                           Current minimum character used by the
+ *							 AES to do vst_height() prior to
+ *							 writing to the screen. This entry is
+ *							 only present as of AES version 0x0400.
+ * </table>
  *
  *  @since All AES versions.
  *
