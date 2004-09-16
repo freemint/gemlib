cdc6b57c8ea5e1fa4d404a1deb6151029e8d3fdb
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2004-09-16 19:21:13 +0000

    - add FA_xxx constants for mt_form_alert icon (moved from windom) - update documentation

--- gemlib/a_form_alert.c
+++ gemlib/a_form_alert.c
@@ -9,44 +9,38 @@
  *
  *  @param default_button contains the number of the exit button which is to
  *               be made default (1-3)
- *  @param alert_string contains a formatted string
- *               as follows:
- *                              "[#][Alert Text][Buttons]".
- *               \n\n
- *               # specifies the icon to display in the alert as follows:
- *               - 0       No Icon
- *               - 1       Exclamation Icon
- *               - 2       Question Icon
- *               - 3       Stop Icon
- *               - 4       Info Icon
- *               - 5       Disk Icon
- *               \n\n
- *               'Alert Text' is a text string of as many as 5 lines composed
- *               of up to 30 characters each. Each line is separated by a '|'
- *               character.
- *               \n\n
- *               'Buttons' is a text string to define as many as 3 buttons
- *               up to 10 characters each. If only one button is used, its
- *               text may be as long as 30 characters. Again, each button is
- *               separated by a '|' character
- *
+ *  @param alert_string contains a formatted string "[x][Alert Text][Buttons]" where
+ *               - x specifies the icon to display in the alert as follows:
+ *               	- 0 (#FA_NOICON)	No Icon
+ *					- 1 (#FA_ERROR)		Exclamation Icon
+ *					- 2 (#FA_QUESTION)	Question Icon
+ *					- 3 (#FA_STOP)		Stop Icon
+ *					- 4 (#FA_INFO)		Info Icon
+ *					- 5 (#FA_DISK)		Disk Icon
+ *               - 'Alert Text' is a text string of as many as 5 lines composed
+ *                 of up to 30 characters each. Each line is separated by a '|'
+ *                 character.
+ *               - 'Buttons' is a text string to define as many as 3 buttons
+ *                 up to 10 characters each. If only one button is used, its
+ *                 text may be as long as 30 characters. Again, each button is
+ *                 separated by a '|' character.
  *  @param global_aes global AES array
  *
- *  @return WORD indicating which button was used
+ *  @return a word indicating which button was used
  *               to exit by the user (A possible value of 1-3).
  *
  *  @since All AES versions. Icons #4-5 are only available as of AES
  *               version 4.1.
  *
  *  Several versions of the AES have special quirks related to
- *  this function. By following the quidelines below you should
+ *  this function. By following the guidelines below you should
  *  avoid any difficulty:
- *  1. All AES versions below 1.06 have some difficulty
+ *  - All AES versions below 1.06 have some difficulty
  *     formatting alert strings padded with spaces.
  *     If you want your alerts to look right on all AES versions,
  *     do not pad any button or line with spaces with the
  *     exception below.
- *  2. Add one space to the end of the longest text line on an
+ *  - Add one space to the end of the longest text line on an
  *     alert. This will prevent the right edge from touching the
  *     border in some AES versions.
  *
