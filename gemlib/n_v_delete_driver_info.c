868700b7265a309e3fdba8c0814d169efc36afec
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2005-02-23 21:10:19 +0000

    New function for the printer dialog from WDialog (fixes)

--- gemlib/n_v_delete_driver_info.c
+++ gemlib/n_v_delete_driver_info.c
@@ -22,12 +22,12 @@ short v_delete_driver_info( short handle, DRV_INFO *drv_info )
 {
 	short vdi_control[VDI_CNTRLMAX]; 
 	short vdi_intin[2]; 
-	short vdi_intout[1]; 
-
-	*(DRV_INFO **) &vdi_intin[0] = drv_info;
+	short vdi_intout[1];
 	
 	VDI_PARAMS(vdi_control, vdi_intin, 0L, vdi_intout, 0L);
-	
+
+	*(DRV_INFO **) &vdi_intin[0] = drv_info;
+		
 	VDI_TRAP (vdi_params, handle, 181, 0,2);
 
 	return vdi_intout[0];
