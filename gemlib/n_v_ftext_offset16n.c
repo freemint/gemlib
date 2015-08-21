7f5d1d84fc97a787227cc3bb05e5030ae2d8dd04
Olivier Landemarre <o.l@lutece.net>
2015-08-21 20:30:08 +0000

    add new text functions using 16bit size char

---
+++ gemlib/n_v_ftext_offset16n.c
@@ -0,0 +1,36 @@
+#include "gem_vdiP.h"
+#include "mt_gemx.h"
+
+/** see v_ftext_offset()
+ *
+ *  @param handle Device handle
+ *  @param x 
+ *  @param y 
+ *  @param wstr 
+ *  @param num string len
+ *  @param offset 
+ *
+ *
+ */
+
+void
+v_ftext_offset16n (short handle, short x, short y,
+                  const WCHAR * wstr, short num, const short * offset)
+{
+	short vdi_control[VDI_CNTRLMAX]; 
+	short vdi_ptsin[VDI_PTSINMAX*2+2];   
+	const long * src = (const long*)offset;
+	long       * dst =       (long*)vdi_ptsin;
+	short     i;
+	
+	if(num>VDI_PTSINMAX) num=VDI_PTSINMAX;
+	VDI_PARAMS(vdi_control, (const short *)wstr, vdi_ptsin, vdi_dummy, vdi_dummy);
+	
+	vdi_ptsin[0] = x;
+	vdi_ptsin[1] = y;
+	for (i = 0; i < num; i++) {
+		*(++dst) = *(src++);
+	}
+
+	VDI_TRAP (vdi_params, handle, 241, (num +1),num);
+}
