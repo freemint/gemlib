13fd339941f9dc26ed28438cfd0bec820a2a080b
Arnaud Bercegeay <arnaud.bercegeay@gmail.com>
2015-08-23 11:21:36 +0000

    add missing documentation (in order to remove doxygen warnings)

--- gemlib/vdi_str2array.c
+++ gemlib/vdi_str2array.c
@@ -7,7 +7,7 @@
 /** 
  *
  *  @param src input string (standard null-terminated C-string)
- *  @param des outpur string (VDI format, each char occupied 16 bits)
+ *  @param des output string (VDI format, each char occupied 16 bits)
  *
  *  @return the len of the string
  *
@@ -26,6 +26,16 @@ vdi_str2array (const char *src, short *des)
 	}
 	return len;
 }
+
+/** 
+ *
+ *  @param src input string (standard null-terminated C-string)
+ *  @param des output string (VDI format, each char occupied 16 bits)
+ *  @param nmax TODO
+ *
+ *  @return the len of the string
+ *
+ */
 
 short
 vdi_str2array_n (const char *src, short *des, int nmax)
