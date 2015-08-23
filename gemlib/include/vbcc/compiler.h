c7ab4093cf4377ae158838fb56176c456d5d50f4
Olivier Landemarre <o.l@lutece.net>
2015-08-23 17:38:19 +0000

    compiler.h for VBCC

---
+++ gemlib/include/vbcc/compiler.h
@@ -0,0 +1,21 @@
+/* compiler.h for vbcc */
+
+#ifndef __COMPILER_H__
+#define __COMPILER_H__ 1
+
+#ifndef cdecl
+#define cdecl
+#endif
+
+#ifndef __CDECL
+#define __CDECL cdecl
+#endif
+
+#ifndef __cdecl
+#define __cdecl cdecl
+#endif
+
+#define __BEGIN_DECLS
+#define __END_DECLS
+
+#endif /* __COMPILER_H__ */
