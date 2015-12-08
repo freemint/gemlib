68ced1c9cab0d7bbdae93bf052c1ea2d09c82ee5
Olivier Landemarre <o.l@lutece.net>
2015-12-08 22:34:40 +0000

    simplification

--- gemlib/vst_scratch.c
+++ gemlib/vst_scratch.c
@@ -23,10 +23,8 @@ void
 vst_scratch (short handle, short mode)
 {
 	short vdi_control[VDI_CNTRLMAX]; 
-	short vdi_intout[VDI_INTOUTMAX]; /* todo (doc wanted) */
-	short vdi_ptsout[VDI_PTSOUTMAX]; /* todo (doc wanted) */
-	
-	VDI_PARAMS(vdi_control, &mode, 0L, vdi_intout, vdi_ptsout );
+
+	VDI_PARAMS(vdi_control, &mode, 0L, vdi_dummy, vdi_dummy );
 	
 	VDI_TRAP (vdi_params, handle, 244, 0,1);
 }
