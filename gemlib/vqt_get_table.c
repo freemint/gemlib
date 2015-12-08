856fa5d877c85f747367d097e6a334927b0969a7
Olivier Landemarre <o.l@lutece.net>
2015-12-08 22:41:43 +0000

    reduce stack use

--- gemlib/vqt_get_table.c
+++ gemlib/vqt_get_table.c
@@ -20,10 +20,9 @@ void
 vqt_get_table (short handle, short **map)
 {
 	short vdi_control[VDI_CNTRLMAX]; 
-	short vdi_intout[VDI_INTOUTMAX]; /* todo (doc wanted) */
-	short vdi_ptsout[VDI_PTSOUTMAX]; /* todo (doc wanted) */
-	
-	VDI_PARAMS(vdi_control, 0L, 0L, vdi_intout, vdi_ptsout );
+	short vdi_intout[4]; /* todo (doc wanted) */
+
+	VDI_PARAMS(vdi_control, 0L, 0L, vdi_intout, vdi_dummy );
 	
 	VDI_TRAP_00 (vdi_params, handle, 254);
 	
