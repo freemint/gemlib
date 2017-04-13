5857318eecd656dbd79035aa1080e16914a6d404
Miro Kropacek <miro.kropacek@gmail.com>
2017-04-13 20:56:19 +1000

    Travis CI deployment

---
+++ .travis/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+TMP="$1"
+OUT="$2"
+
+make CROSS=yes
+
+make CROSS=yes PREFIX="${TMP}" install
+find "{$TMP}" -type f -perm -a=x -exec m68k-atari-mint-strip -s {} \;
+find "{$TMP}" -type f \( -name '*.a' -o -name '*.o' \) -exec m68k-atari-mint-strip -S -x {} \;
+mkdir -p "${OUT}" && cd "${TMP}" && tar cjf "${OUT}/${PROJECT}-${SHORT_ID}.tar.bz2" *
