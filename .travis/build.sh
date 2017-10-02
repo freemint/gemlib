c0f7e5d3fb3b987f6bc8bc6cbfd9233dff7886f5
Miro Kropacek <miro.kropacek@gmail.com>
2017-10-03 08:45:11 +1000

    Update build.sh
    - build.sh should report first failure immediately
    - fix the find command

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 TMP="$1"
 OUT="$2"
@@ -6,6 +6,6 @@ OUT="$2"
 make CROSS=yes
 
 make CROSS=yes PREFIX="${TMP}" install
-find "{$TMP}" -type f -perm -a=x -exec m68k-atari-mint-strip -s {} \;
-find "{$TMP}" -type f \( -name '*.a' -o -name '*.o' \) -exec m68k-atari-mint-strip -S -x {} \;
+find "${TMP}" -type f -perm -a=x -exec m68k-atari-mint-strip -s {} \;
+find "${TMP}" -type f \( -name '*.a' -o -name '*.o' \) -exec m68k-atari-mint-strip -S -x {} \;
 mkdir -p "${OUT}" && cd "${TMP}" && tar cjf "${OUT}/${PROJECT}-${SHORT_ID}.tar.bz2" *
