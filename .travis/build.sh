59e3d9972568ea48a20b93ce4438df9872d789a7
Thorsten Otto <admin@tho-otto.de>
2019-10-04 13:33:05 +0200

    Autodetect CROSS compilation

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,9 +3,9 @@
 TMP="$1"
 OUT="$2"
 
-make CROSS=yes
+make
 
-make CROSS=yes PREFIX="${TMP}" install
+make PREFIX="${TMP}" install
 find "${TMP}" -type f -perm -a=x -exec m68k-atari-mint-strip -s {} \;
 find "${TMP}" -type f \( -name '*.a' -o -name '*.o' \) -exec m68k-atari-mint-strip -S -X -w -N '.L[0-9]*' {} \;
 mkdir -p "${OUT}" && cd "${TMP}" && tar cjf "${OUT}/${PROJECT}-${SHORT_ID}.tar.bz2" *
