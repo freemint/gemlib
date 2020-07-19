28307329cfde80bf7ac7ca61c8b84e70dc11945c
Thorsten Otto <admin@tho-otto.de>
2020-07-19 14:52:14 +0200

    Implement vqt_extentn

---
+++ vqt_extentn.c
@@ -0,0 +1,22 @@
+#include "gem_vdiP.h"
+
+/** see vqt_extent()
+ *
+ *  @param handle Device handle
+ *  @param wstr string in 16-bit format (16 bits per character)
+ *  @param num length of the string
+ *  @param extent coordinates of the rectangle
+ *
+ */
+
+void
+vqt_extentn (short handle, const char *str, short num, short extent[])
+{
+	short vdi_control[VDI_CNTRLMAX]; 
+	short vdi_intin[VDI_INTINMAX];   
+
+	num = vdi_str2array_n (str, vdi_intin, num);
+	VDI_PARAMS(vdi_control, vdi_intin, 0L, vdi_dummy, extent );
+	
+	VDI_TRAP (vdi_params, handle, 116, 0,num);
+}
