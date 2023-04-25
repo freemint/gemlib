f76a6c5b5ef73434a60ac0f92d81b0c39d04b7b3
Thorsten Otto <admin@tho-otto.de>
2023-04-25 11:49:54 +0200

    Implement x_sprintf binding

---
+++ x_sprintf.c
@@ -0,0 +1,34 @@
+#include "gem_aesP.h"
+#include "mt_gemx.h"
+#include "gem.h"
+#include <stdarg.h>
+
+/** Print data to a string, with extended formats.
+ *
+ *  @param buf
+ *  @param fmt
+ *  @param global_aes global AES array
+ *
+ *  @since Geneva
+ *
+ */
+
+/* this only available with -mshort, because otherwise size of int does not match */
+#ifdef __MSHORT__
+#define global_aes aes_global
+void
+mt_x_sprintf(char *buf, const char *fmt, ...)
+{
+	va_list args;
+	AES_PARAMS(0x7183,0,0,3,0);
+
+	va_start(args, fmt);
+	aes_addrin[0] = (long)buf;
+	aes_addrin[1] = (long)fmt;
+	aes_addrin[2] = (long)args;
+	va_end(args);
+
+	AES_TRAP(aes_params);
+}
+#undef global_aes
+#endif
