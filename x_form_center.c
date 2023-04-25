37cbcea92538a84a9e3fded7b087809454b5a8b0
Thorsten Otto <admin@tho-otto.de>
2023-04-25 08:21:35 +0200

    Implement x_form_error binding

--- x_form_center.c
+++ x_form_center.c
@@ -3,16 +3,24 @@
 
 /** Center a dialog on the screen, taking into account all object attributes that can affect the outer border.
  *
- *  @param tree
- *  @param cx
- *  @param cy
- *  @param cw
- *  @param ch
+ *  @param tree points to a valid #OBJECT structure which the
+ *               application wishes to have centered.
+ *  @param cx,cy,cw,ch is a clipping rectangle suitable for
+ *               use in objc_draw() returned.
+ *               [option CHECK_NULLPTR] \a cx, \a cy, \a cw and/or \a ch may be NULL
  *  @param global_aes global AES array
  *
- *  @return 0 if an error has occurred
+ *  @return is currently reserved. Currently it equals 1.
  *
  *  @since Geneva
+ *
+ *  @sa mt_objc_draw()
+ *
+ *  The values that mt_form_center() returns in \a cx, \a cy, \a cw, and \a ch,
+ *  are not necessarily the same as the object's. These values
+ *  take into account negative borders, outlining, and
+ *  shadowing. This is meant to provide a suitable clipping
+ *  rectangle for mt_objc_draw()
  *
  */
 
@@ -25,10 +33,22 @@ mt_x_form_center(OBJECT *tree, short *cx, short *cy, short *cw, short *ch, short
 
 	AES_TRAP(aes_params);
 
-	*cx = aes_intout[0];
-	*cy = aes_intout[1];
-	*cw = aes_intout[2];
-	*ch = aes_intout[3];
+#if CHECK_NULLPTR
+	if (cx)
+#endif
+	*cx = aes_intout[1];
+#if CHECK_NULLPTR
+	if (cy)
+#endif
+	*cy = aes_intout[2];
+#if CHECK_NULLPTR
+	if (cw)
+#endif
+	*cw = aes_intout[3];
+#if CHECK_NULLPTR
+	if (ch)
+#endif
+	*ch = aes_intout[4];
 
-	return (aes_intout[0]);
+	return aes_intout[0];
 }
---
+++ x_form_error.c
@@ -0,0 +1,78 @@
+#include "gem_aesP.h"
+#include "mt_gemx.h"
+
+/** displays a pre-defined error alert box to the user.
+ *
+ *  @param fmt
+ *  @param error_code specifies a MS-DOS error code as follows:
+ *               - #FERR_FILENOTFOUND : File Not Found
+ *                                             The application can not find
+ *                                             the folder or file that you
+ *                                             tried to access.
+ *               - #FERR_PATHNOTFOUND :  Path Not Found
+ *                                             The application cannot find
+ *                                             the folder or file that you
+ *                                             tried to access.
+ *               - #FERR_NOHANDLES  :  No More File Handles
+ *                                             The application does not
+ *                                             have room to open another
+ *                                             document. To make room, close
+ *                                             any open document that you do
+ *                                             not need.
+ *               - #FERR_ACCESSDENIED : Access Denied
+ *                                             An item with this name
+ *                                             already exists in the
+ *                                             directory, or this item is
+ *                                             set to read-only status.
+ *               - #FERR_LOWMEM :      Insufficient Memory
+ *                                             There is not enough memory
+ *                                             for the application you just
+ *                                             tried to run.
+ *               - #FERR_BADENVIRON  : Invalid Environment
+ *                                             There is not enough memory
+ *                                             for the application you just
+ *                                             tried to run.
+ *               - #FERR_BADFORMAT :   Invalid Format
+ *                                             There is not enough memory
+ *                                             for the application you just
+ *                                             tried to run.
+ *               - #FERR_BADDRIVE :   Invalid Drive Specification
+ *                                             The drive you specified does
+ *                                             not exist.
+ *               - #FERR_DELETEDIR :   Attempt To Delete Working
+ *                                             Directory
+ *                                             You cannot delete the folder
+ *                                             in which you are working.
+ *               - #FERR_NOFILES :     No More Files
+ *                                             The application can not find
+ *                                             the folder or file that you
+ *                                             tried to access.
+ *  @param global_aes global AES array
+ *
+ *  @return the exit button clicked as in
+ *          mt_form_alert(). It is, however, insignifigant as all of the
+ *          error alerts have only one button.
+ *
+ *  @since All AES versions.
+ *
+ *  @sa mt_form_alert()
+ *
+ *  @note The GEMDOS error number can be translated into the code expected
+ *        in \a error_code by subtracting 31 from the absolute value of the
+ *        GEMDOS error code.
+ *  @note Not every GEMDOS error code has a matching alert box.
+ *
+ */
+
+short
+mt_x_form_error(const char *fmt, short error_code, short *global_aes)
+{
+	AES_PARAMS(0x7111,1,1,1,0);
+
+	aes_intin[0] = error_code;
+	aes_addrin[0] = (long)fmt;
+
+	AES_TRAP(aes_params);
+
+	return aes_intout[0];
+}
