2d8479ec6aee063c6febcf3063339fb19700cc93
Thorsten Otto <admin@tho-otto.de>
2023-04-25 12:28:48 +0200

    Implement x_mfree binding

--- x_mfree.c
+++ x_mfree.c
@@ -1,23 +1,25 @@
 #include "gem_aesP.h"
 #include "mt_gemx.h"
 
-/** Allocate memory from Geneva. 
+/** Free memory allocated with x_malloc.
  *
  *  @param addr
- *  @param size
  *  @param global_aes global AES array
+ *
+ *  @return 0 if an error has occurred
  *
  *  @since Geneva
  *
  */
 
-void
-mt_x_malloc(void **addr, long size, short *global_aes)
+short
+mt_x_mfree(void *addr, short *global_aes)
 {
-	AES_PARAMS(0x7186,2,0,1,0);
+	AES_PARAMS(0x7187,0,1,1,0);
 
-	aes_intin_long(0) = size;
 	aes_addrin[0] = (long)addr;
 
 	AES_TRAP(aes_params);
+
+	return aes_intout[0];
 }
