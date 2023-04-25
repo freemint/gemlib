3944d8c8d99bf182687a3ab9a24cc9c8e58efeae
Thorsten Otto <admin@tho-otto.de>
2023-04-25 10:42:38 +0200

    Implement x_wdial_draw binding

---
+++ x_wdial_draw.c
@@ -0,0 +1,29 @@
+#include "gem_aesP.h"
+#include "mt_gemx.h"
+
+/** Redraw a dialog within a window.
+ *
+ *  @param handle
+ *  @param statr
+ *  @param depth
+ *  @param global_aes global AES array
+ *
+ *  @return 0 if an error occured or non-zero otherwise.
+ *
+ *  @since Geneva
+ *
+ */
+
+short
+mt_x_wdial_draw(short handle, short start, short depth, short *global_aes)
+{
+	AES_PARAMS(0x7150,3,1,0,0);
+
+	aes_intin[0] = handle;
+	aes_intin[1] = start;
+	aes_intin[2] = depth;
+
+	AES_TRAP(aes_params);
+
+	return aes_intout[0];
+}
