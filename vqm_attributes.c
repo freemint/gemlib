6eb35a21030127714a80857ebf590ea7d3eaa8c5
Thorsten Otto <admin@tho-otto.de>
2023-06-16 07:58:42 +0200

    Fix binding of vqm_attributes
    Marker height must be returned in attrib[3], therefor we
    cannot use it directly as input parameter for the ptsout array

--- vqm_attributes.c
+++ vqm_attributes.c
@@ -3,7 +3,7 @@
 /** reports back the current marker attributes
  *
  *  @param handle Device handle
- *  @param attrib 
+ *  @param attrib
  *         - attrib[0]           marker type
  *         - attrib[1]           marker color
  *         - attrib[2]           writing mode
@@ -17,9 +17,13 @@
 void
 vqm_attributes (short handle, short attrib[])
 {
-	short vdi_control[VDI_CNTRLMAX]; 
+	short vdi_control[VDI_CNTRLMAX];
+	short ptsout[2];
 
-	VDI_PARAMS(vdi_control, 0L, 0L, attrib, &attrib[3] );
-	
+	VDI_PARAMS(vdi_control, 0L, 0L, attrib, ptsout);
+
 	VDI_TRAP_00 (vdi_params, handle, 36);
+
+	attrib[3] = ptsout[1]; /* marker height */
+	attrib[4] = ptsout[0]; /* marker width; unused */
 }
