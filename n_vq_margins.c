aedb7244572b5553736f2d710f5a257eb4d99788
Thorsten Otto <admin@tho-otto.de>
2024-08-26 16:28:57 +0200

    Workaround a bug in vq_margins() of NVDI 5.x drivers

--- n_vq_margins.c
+++ n_vq_margins.c
@@ -30,6 +30,12 @@ vq_margins (short handle, short *top_margin, short *bottom_margin,
 	VDI_PARAMS(vdi_control, 0L, 0L, vdi_intout, vdi_dummy);
 	
 	vdi_intout[0] = 0;
+	vdi_intout[1] = 0;
+	vdi_intout[2] = 0;
+	vdi_intout[3] = 0;
+	vdi_intout[4] = 0;
+	vdi_intout[5] = 0;
+	vdi_intout[6] = 0;
 
 	VDI_TRAP_ESC (vdi_params, handle, 5,2100, 0,0);
 
@@ -39,6 +45,30 @@ vq_margins (short handle, short *top_margin, short *bottom_margin,
 	*right_margin  = vdi_intout[4];
 	*hdpi          = vdi_intout[5];
 	*vdpi          = vdi_intout[6];
+
+	/*
+	 * NVDI 5.x printer drivers have a bug, and write some output parameters
+	 * directly to the VDIPB instead of the intout array.
+	 * Because of another bug in the same routine,
+	 * it also does not return the value of vdpi
+	 */
+	if (vdi_params.control != vdi_control)
+	{
+		short *pb;
+
+		pb = (short *)&vdi_params;
+		/* top_margin is ok */
+		/* bottom_margin is ok */
+		/* left_margin was written here */
+		*left_margin = pb[0];
+		/* right_margin was written here */
+		*right_margin = pb[1];
+		/* hdpi was written here */
+		*hdpi = pb[2];
+		/* vdpi is not accessible; pb[3] will contain the y-resolution */
+		/* assume vdpi == hdpi */
+		*vdpi = pb[2];
+	}
 
 	return vdi_intout[0];
 }
